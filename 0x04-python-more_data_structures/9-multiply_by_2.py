753eae9f16d269669c61d2a3e88a9f5a2ce3148c
Ballans <maballans@gmail.com>
2024-01-11 06:31:58 -0800

    Task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_ = a_dictionary.copy()
+    list_ = list(new_dir.keys())
+
+    for i in list_:
+        new_dir[i] *= 2
+
+    return (new_)
