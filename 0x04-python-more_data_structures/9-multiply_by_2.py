b401a2e5f0e392f2521ae874a778b72cd05ea45e
Ballans <maballans@gmail.com>
2024-01-11 07:23:59 -0800

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir= a_dictionary.copy()
-    list_keys= list(new_dir.keys())
+    """
+    Returns a new dictionary with all values multiplied by 2.
 
-    for i in list_:
-        new_dir[i] *= 2
+    Parameters:
+        a_dictionary (dict): The input dictionary with integer values.
 
-    return (new_dir)
+    Returns:
+        dict: A new dictionary with all values multiplied by 2.
+    """
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
