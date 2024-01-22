9a9083de87611bb5e8206afc462b63187684ade3
Ballans <maballans@gmail.com>
2024-01-22 11:50:13 -0800

    Task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,4 +2,5 @@ g!/usr/bin/python3
 
 def raise_exception_msg(message=""):
     """Raising a NameError exception, but with a message this time."""
+    message = "Error"
     raise NameError(message)
