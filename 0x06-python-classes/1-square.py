843be4bf11f512f62f797dd5cc5480336ff4ebd1
Ballans <maballans@gmail.com>
2024-01-23 11:59:54 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defining a new class Square."""
+
+
+class Square:
+    """Representing a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.__size = size
+
