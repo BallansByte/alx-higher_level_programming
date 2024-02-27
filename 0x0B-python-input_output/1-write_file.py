7e0b0466f7eafd75b0d21669221636fa5c52e60f
Ballans <kofikoo68@gmail.com>
2024-02-27 04:42:46 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,16 @@
+1-write_file.py
 #!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
 def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
-    """
-    char_count = 0
-    with open(filename, 'w', encoding='utf-8') as f:
-        char_count = f.write(text)
-    return char_count
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
