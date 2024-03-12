c7a4d8c45f540cc80fb2a01ec7af3625dc985461
Ballans <maballans@gmail.com>
2024-03-12 15:15:00 -0700

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Script to print the full description of table first_table
 
 -- Query table information from information_schema
-SHOW CREATE TABLE  'first_table';
+SHOW CREATE TABLE  first_table;
