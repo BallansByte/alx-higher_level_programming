bfedd467ed4d7a3775359f46c39ebcb153a6f68b
Ballans <maballans@gmail.com>
2024-03-12 14:58:11 -0700

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Script to print the full description of table first_table
 
 -- Query table information from information_schema
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE  'first_table';
