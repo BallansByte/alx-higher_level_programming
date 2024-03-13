5e8bde974c00dd0ea372eb958757311910aef0a2
Ballans <maballans@gmail.com>
2024-03-13 05:19:24 -0700

    Task 12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,9 @@
+-- SQL script to update Bob's score to 10 in the second_table
+
+-- Connect to your MySQL database
+
+-- Update Bob's score to 10
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
