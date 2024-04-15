065f0c091a699912c2841a44d7b20c5da33e5455
Ballans <maballans@gmail.com>
2024-04-15 10:54:27 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,11 @@
 #!/usr/bin/python3
+"""Starting a linking class to table in database
 """
-Taking the name of a state
-as an argument and listing all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
+import sys
+from model_state import Base, State
 
-import MySQLdb as db
-from sys import argv
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    """
-    Accessing to the database and get the cities
-    from the database.
-    """
-
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-        rows_selected = db_cursor.fetchall()
-
-    if rows_selected is not None:
-        print(", ".join([row[1] for row in rows_selected]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
