7731285c4c765126019a9c9e43acbeb6fd9069e3
Ballans <maballans@gmail.com>
2024-04-15 04:56:48 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,13 @@
+
+#!/usr/bin/python3
+# Listing all states with a name starting with N from the database .
+# Usage: ./1-filter_states.py <mysql username> \
+
 import sys
 import MySQLdb
 
-def search_state(username, password, database, state_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-
-    # Create SQL query using format
-    query = "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC".format(state_name)
-
-    # Execute the query
-    cursor.execute(query)
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
-        print(row)
-
-    # Close cursor and connection
-    cursor.close()
-    db.close()
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    search_state(username, password, database, state_name)
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
