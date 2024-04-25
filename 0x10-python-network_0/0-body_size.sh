7db87a6ce62385673e7976866cb368e3c17db2ec
Ballans <maballans@gmail.com>
2024-04-25 13:23:52 -0700

    Task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes a URL, sends request to that URL, and displays the size of the body of the response
-curl -sX GET $1 -L#
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
