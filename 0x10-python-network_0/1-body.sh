7da1242ebefa2e2243962e4bb99524e553273f7f
Ballans <maballans@gmail.com>
2024-04-25 13:41:19 -0700

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a given URL and displays the body of the response if the status code is 200
-curl -s -o response.txt -w "%{http_code}" $1 | grep -q "^200$" && cat response.txt && rm response.txt || rm response.txt
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
