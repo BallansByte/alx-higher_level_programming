695fa10608157b1d79326c00a6aff40cee548b49
Ballans <maballans@gmail.com>
2024-04-26 08:46:44 -0700

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -5,11 +5,7 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
-
-# Assign the URL from the first argument
 URL=$1
-
-# Use curl to send a POST request with variables email and subject
 curl -s -X POST $URL \
     -d "email=test@gmail.com" \
     -d "subject=I will always be here for PLD"
