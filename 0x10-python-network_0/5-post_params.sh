94ea28420d760e5fa393400ad2bbcfa3afcb649b
Ballans <maballans@gmail.com>
2024-04-29 04:12:58 -0700

    Task 8

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Script to send a POST request and display the body of the response
-
-url="$1" # Get the URL from the first command line argument
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-curl -s -X POST "$url" -d "email=$email&subject=$subject"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
