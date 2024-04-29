1f176b4c48b54f44599dbdce8cd26cf41bcc4024
Ballans <maballans@gmail.com>
2024-04-29 04:51:18 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,8 @@
 This Python script takes a URL from the command line, sends a request to that URL,
 and prints the value of the 'X-Request-Id' header found in the HTTP response.
 """
-
 import urllib.request
 import sys
-
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
