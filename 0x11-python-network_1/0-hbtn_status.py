3455fecf2caf64602b3c4866b39ac232ec89ca00
Ballans <maballans@gmail.com>
2024-04-29 04:39:53 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script fetches data from 'https://alx-intranet.hbtn.io/status' using the urllib package.
+It uses only the urllib library to make the HTTP request and prints the body of the response
+in a specific formatted way as required.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+
+    with urllib.request.urlopen(url) as response:
+
+        content = response.read()
+
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
