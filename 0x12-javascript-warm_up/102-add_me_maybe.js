09eeeaa54984db1158686d8a4ddac051fd65b5da
Ballans <maballans@gmail.com>
2024-05-07 11:11:46 -0700

    Task 102

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Increments a number and calls a callback function with the incremented value.
+ * @param {number} number The number to increment.
+ * @param {Function} theFunction The callback function to call with the incremented number.
+ */
+const addMeMaybe = (number, theFunction) => {
+    theFunction(number + 1);
+};
+
+module.exports.addMeMaybe = addMeMaybe;
