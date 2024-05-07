268f0ac96c08a414c57470aef0ec680fee8e5ecb
Ballans <maballans@gmail.com>
2024-05-07 09:32:47 -0700

    Task ls AMendment

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
-function add (a, b) {
-	return a + b;
-}
-module.exports.add = add;
+
+/**
+ * This function adds two integers.
+ * @param {number} a The first integer.
+ * @param {number} b The second integer.
+ * @returns {number} The sum of the two integers.
+ */
+const add = (a, b) => {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        return a + b;
+    } else {
+        throw new Error('Both arguments must be integers');
+    }
+};
+
+module.exports = add;
