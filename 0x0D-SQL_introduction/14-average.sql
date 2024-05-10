a18a804d305ca7456131dcac98a4ae854aae2197
BallansByte <maballans@gmail.com>
2024-05-10 11:54:11 -0700

    Task 14 Amend

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,12 +1,3 @@
-#!/bin/bash
-# This script calculates the average score from the table second_table in the hbtn_0c_0 database.
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DATABASE=$1
-
-SQL="SELECT AVG(score) AS average FROM second_table;"
-mysql -u username -p --database="$DATABASE" -e "$SQL"
+-- Print the average score
+-- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) AS average FROM second_table;
