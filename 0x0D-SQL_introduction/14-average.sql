c431f380051a257ef672184bc57784e7a6378a90
BallansByte <maballans@gmail.com>
2024-05-10 11:36:22 -0700

    Task 14 Amend

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,12 @@
---computing the average score of all records in the table second_table.
-SELECT AVG(`score`) AS `average`
-FROM 'second_table';
+#!/bin/bash
+# This script calculates the average score from the table second_table in the hbtn_0c_0 database.
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+DATABASE=$1
+
+SQL="SELECT AVG(score) AS average FROM second_table;"
+mysql -u username -p --database="$DATABASE" -e "$SQL"
