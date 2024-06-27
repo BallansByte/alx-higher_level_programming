f2c7df578652d82dcf6f7b8f7cfa08b6cac3e238
BallansByte <Maballans@gmail.com>
2024-06-27 07:48:10 -0700

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const [,, filePath, content] = process.argv;
 
-/*This module checks if the file path is there or the content to be copied is available. 
-If its not it brings ans error*/
-if (!filePath || !content) {
-  console.error('Usage: ./writeToFile.js <file path> <string to write>');
-  process.exit(1);
-}
-/*Code to write ther file and return any error if any */
-fs.writeFile(filePath, content, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
-  console.log('File has been written');
 });
